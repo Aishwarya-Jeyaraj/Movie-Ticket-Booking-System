a8a263d1b687db380fa5aff398d2a9f4e4a0941a
Aishwarya-Jeyaraj <82449060+Aishwarya-Jeyaraj@users.noreply.github.com>
2022-08-03 23:50:35 +0530

    Add files via upload

---
+++ Movie Ticket Booking System/Controllers/Driver/ControllerDriver.cs
@@ -0,0 +1,21 @@
+﻿using MovieTicketBookingSystem_V3.Controllers.Interface;
+
+namespace MovieTicketBookingSystem_V3.Controllers.Driver
+{
+    public class ControllerDriver
+    {
+        private readonly IController _controller;
+        public ControllerDriver(IController controller)
+        {
+            _controller = controller;
+        }
+        public Dictionary<int,string> ExecuteDisplayAction()
+        {
+           return _controller.ExecuteDisplayAction();
+        }
+        public void ExecuteSelectAction(int i)
+        {
+            _controller.ExecuteSelectAction(i);
+        }
+    }
+}
