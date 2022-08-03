b3703ed0501031333b5a04a8cba66df4b2209175
Aishwarya-Jeyaraj <82449060+Aishwarya-Jeyaraj@users.noreply.github.com>
2022-08-03 23:57:15 +0530

    Add files via upload

---
+++ Movie Ticket Booking System/Views/Driver/ViewDriver.cs
@@ -0,0 +1,21 @@
+﻿using MovieTicketBookingSystem_V3.Views.Interface;
+
+namespace MovieTicketBookingSystem_V3.Views.Driver
+{
+    public class ViewDriver
+    {
+        private IView _view;
+        public ViewDriver(IView view)
+        {
+            _view = view;
+        }
+        public void RenderDisplayAction()
+        {
+            _view.RenderDisplayAction();
+        }
+        public void RenderSelectAction()
+        {
+            _view.RenderSelectAction();
+        }
+    }
+}
