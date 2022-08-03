4b00887b13774f9e4f5e67fb98993885cbbf7289
Aishwarya-Jeyaraj <82449060+Aishwarya-Jeyaraj@users.noreply.github.com>
2022-08-03 23:58:24 +0530

    Add files via upload

---
+++ Movie Ticket Booking System/Views/Interface/IView.cs
@@ -0,0 +1,11 @@
+﻿using MovieTicketBookingSystem_V3.Controllers.Driver;
+
+namespace MovieTicketBookingSystem_V3.Views.Interface
+{
+    public interface IView
+    {
+        protected static ControllerDriver controllerDriver;
+        public void RenderDisplayAction();
+        public void RenderSelectAction();
+    }
+}
