0ab7d8d2770ba5fad00e617719b18c0a20472425
Aishwarya-Jeyaraj <82449060+Aishwarya-Jeyaraj@users.noreply.github.com>
2022-08-03 23:51:13 +0530

    Add files via upload

---
+++ Movie Ticket Booking System/Controllers/Interface/IController.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieTicketBookingSystem_V3.Controllers.Interface
+{
+    public interface IController
+    {
+        public Dictionary<int,string> ExecuteDisplayAction();
+        public void ExecuteSelectAction(int i);
+
+    }
+}
